313e9c74fa722d0e070e6e369973f5a8b5fdd4c8
Brandon Grimshaw <brandon@pathfindermobilesoftware.com.au>
2014-12-21 01:56:41 +1000

    Initial commit. Basic example working.

---
+++ Assets/src/CubeTester.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using System.Collections;
+
+public class CubeTester : MonoBehaviour {
+
+	public KeyCode XToggleCode;
+	public KeyCode YToggleCode;
+
+	public List<GameObject> Cubes;
+
+	public int x = 0, y = 0;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(XToggleCode)) {
+			x = (x + 1) % 3;
+		}
+
+		if (Input.GetKeyDown(YToggleCode)) {
+			y = (y + 1) % 3;
+		}
+
+		var cubes = Multitag.GetGameObjectsWithTags(new String[] { 'x' + x.ToString(), 'y' + y.ToString() });
+		foreach (var cube in cubes) {
+			cube.GetComponent<MeshRenderer>().enabled = true;
+		}
+
+		foreach (var cube in Cubes.Where(that => !cubes.Contains(that))) {
+			cube.GetComponent<MeshRenderer>().enabled = false;
+		}
+	}
+}
---
+++ Assets/src/Multitag.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using System.Collections;
+
+public class Multitag : MonoBehaviour {
+
+	public List<string> TagsList;
+	
+	public static IEnumerable<GameObject> GetGameObjectsWithTag(string tag) {
+		return (
+				from obj in FindObjectsOfType<Multitag>() 
+				where obj.TagsList.Contains(tag) 
+				select obj.gameObject
+			).ToList(); 
+	}
+
+	public static IEnumerable<GameObject> GetGameObjectsWithTags(IEnumerable<string> tags) {
+		return (
+				from obj in FindObjectsOfType<Multitag>() 
+				where tags.All(x => obj.TagsList.Contains(x)) 
+				select obj.gameObject
+			).ToList();
+	}
+}
